fce3e767a5aa3021adaa696174fdcc2f4a041b27
Alberto Perez <albertojperezf@gmail.com>
2017-11-02 16:34:25 +0100

    feat: finish the project

--- src/components/App.js
+++ src/components/App.js
@@ -6,17 +6,53 @@ import CalendarIcon from 'react-icons/lib/fa/calendar-plus-o'
 
 class App extends Component {
     render() {
-        console.log(this.props)
+        const { calendar, remove } = this.props
+        const mealOrder = ['breakfast', 'lunch', 'dinner']
+
+
         return (
-            <div>
-                Hello World
+            <div className='container'>
+
+                <ul className='meal-types'>
+                    {mealOrder.map((mealType) => (
+                        <li key={mealType} className='subheader'>
+                            {capitalize(mealType)}
+                        </li>
+                    ))}
+                </ul>
+
+                <div className='calendar'>
+                    <div className='days'>
+                        {calendar.map(({ day }) => <h3 key={day} className='subheader'>{capitalize(day)}</h3>)}
+                    </div>
+                    <div className='icon-grid'>
+                        {calendar.map(({ day, meals }) => (
+                            <ul key={day}>
+                                {mealOrder.map((meal) => (
+                                    <li key={meal} className='meal'>
+                                        {meals[meal]
+                                            ? <div className='food-item'>
+                                                <img src={meals[meal].image} alt={meals[meal].label}/>
+                                                <button onClick={() => remove({meal, day})}>Clear</button>
+                                            </div>
+                                            : <button className='icon-btn'>
+                                                <CalendarIcon size={30}/>
+                                            </button>}
+                                    </li>
+                                ))}
+                            </ul>
+                        ))}
+                    </div>
+                </div>
+
             </div>
         )
     }
 }
 
-function mapStateToProps ({food, calendar}) {
+function mapStateToProps ({ food, calendar }) {
     const dayOrder = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday']
+
     return {
         calendar: dayOrder.map((day) => ({
             day,
